2e23543d4f510d2e56549f3893e6481be45f914e
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-12-23 10:47:35 -0600

    used optional chaining to check for empty string or null input value. created task object with id, title, completed, and createdAt properties

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import { v4 as uuidV4 } from 'uuid'
+import { v4 as uuidV4 } from 'uuid'
 
 // console.log(uuidV4())
 
@@ -6,3 +6,17 @@ const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.getElementById("new-task-form") as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
 
+// submit event take in e object
+form?.addEventListener("submit", e => {
+    e.preventDefault()
+    //optional chaining by adding ?
+    if(input?.value == "" || input?.value == null ) return
+    // to-do object
+    const task = {
+        id: uuidV4(),
+        title: input.value,
+        completed: false, 
+        createdAt: new Date()
+    }
+    input.value
+})
